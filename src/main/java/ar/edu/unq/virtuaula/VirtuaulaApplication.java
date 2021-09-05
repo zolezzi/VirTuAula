2df2a8c93f86bfb8d6596100f45b7d492d459b0f
zolezzi <cezcardozo23@gmail.com>
2021-09-05 11:47:56 -0300

    Se agrega proyecto backend con springboot, gradle y spring security

---
+++ src/main/java/ar/edu/unq/virtuaula/VirtuaulaApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.virtuaula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtuaulaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtuaulaApplication.class, args);
+	}
+
+}
