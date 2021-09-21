f3a26598342475faf519c0c8fc29995972fdccc4
Ger121190 <gmcabrera121190@gmail.com>
2021-09-21 17:08:24 -0300

    Configuro cors.

--- src/main/java/ar/edu/unq/virtuaula/config/WebSecurityConfig.java
+++ src/main/java/ar/edu/unq/virtuaula/config/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package ar.edu.unq.virtuaula.config;
 
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -11,11 +16,24 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().configurationSource(corsConfigurationSource());
         http
                 .authorizeRequests()
                 .antMatchers("/api/classrooms").permitAll()
                 .antMatchers("/api/lessons/**").permitAll()
                 .antMatchers("/api/tasks/**").permitAll()
                 .anyRequest().authenticated();
+    }
+    
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","PATCH","DELETE"));
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedHeader("*");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
