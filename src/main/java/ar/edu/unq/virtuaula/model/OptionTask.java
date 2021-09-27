f9f299970256a4b43d543d16633b6874218351fb
zolezzi <cezcardozo23@gmail.com>
2021-09-27 01:02:32 -0300

    fix code style

--- src/main/java/ar/edu/unq/virtuaula/model/OptionTask.java
+++ src/main/java/ar/edu/unq/virtuaula/model/OptionTask.java
@@ -3,7 +3,6 @@ package ar.edu.unq.virtuaula.model;
 import java.io.Serializable;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
