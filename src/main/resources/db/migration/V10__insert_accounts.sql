98522604097e2fe0cde63dd1f806f9fa9670639a
zolezzi <cezcardozo23@gmail.com>
2021-11-02 23:49:50 -0300

    Added new table level

---
+++ src/main/resources/db/migration/V10__insert_accounts.sql
@@ -0,0 +1,9 @@
+INSERT INTO account(username, first_name, last_name, email, account_type_id, account_type_role, experience, user_id, dni, level_id) VALUES ('charly','Carlos', 'Cardozo', 'carlos@gmail.com', 2, 'TeacherAccount', null, 1,35000111, null);
+INSERT INTO account(username, first_name, last_name, email, account_type_id, account_type_role, experience, user_id, dni, level_id) VALUES ('german','German', 'Cabrera', 'german@gmail.com', 1, 'StudentAccount', 0, 2, 35000112, 1);
+
+INSERT INTO teachers_students(teacher_id, student_id) VALUES(1,2);
+
+INSERT INTO accounts_classrooms(account_id, classroom_id) VALUES(1,1);
+INSERT INTO accounts_classrooms(account_id, classroom_id) VALUES(1,2);
+INSERT INTO accounts_classrooms(account_id, classroom_id) VALUES(2,1);
+INSERT INTO accounts_classrooms(account_id, classroom_id) VALUES(2,2);
