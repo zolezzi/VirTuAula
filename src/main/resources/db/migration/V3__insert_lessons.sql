a94660617cdd6243a28b06ab99b033bddd6530d5
Germán Maximiliano Cabrera <gmcabrera121190@gmail.com>
2021-11-07 14:55:45 -0300

    Modifico fecha de vencimiento de las lessons.

--- src/main/resources/db/migration/V3__insert_lessons.sql
+++ src/main/resources/db/migration/V3__insert_lessons.sql
@@ -1,6 +1,6 @@
-﻿INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Ecuaciones', 10, 1, '2021-10-07 23:59:29');
-INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Funciones', 10, 1, '2021-10-07 23:59:29');
-INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Derivadas', 10, 1, '2021-10-07 23:59:29');
-INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Primera Guerra Mundial', 10, 2, '2021-10-07 23:59:29');
-INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Segunda Guerra Mundial', 10, 2, '2021-10-07 23:59:29');
-INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Guerra Fría', 10, 2, '2021-10-07 23:59:29');
+﻿INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Ecuaciones', 10, 1, '2021-12-31 23:59:29');
+INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Funciones', 10, 1, '2021-12-31 23:59:29');
+INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Derivadas', 10, 1, '2021-12-31 23:59:29');
+INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Primera Guerra Mundial', 10, 2, '2021-12-31 23:59:29');
+INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Segunda Guerra Mundial', 10, 2, '2021-12-31 23:59:29');
+INSERT INTO lesson (name, max_note, classroom_id, delivery_date) values ('Guerra Fría', 10, 2, '2021-12-31 23:59:29');
